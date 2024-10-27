4c781358093af9d15c15ea8000e5701fc0d3eb05
cathuyenphan <111101271+cathuyenphan@users.noreply.github.com>
2024-10-27 16:40:04 -0700

    Create A_heuristics.h

---
+++ A_heuristics.h
@@ -0,0 +1,19 @@
+#ifndef A_HEURISTICS_H_
+#define A_HEURISTICS_H_
+
+#include "puzzle.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+cont vector<vector<int>> goalState = {{1,2,3},{4,5,6},{7,8,9}};
+
+int misplacedTile(vector<vector<int>> puzzle)  {
+}
+
+int euclideanDistance(veector<vector<int>> puzzle)  {
+
+}
+
+#endif
