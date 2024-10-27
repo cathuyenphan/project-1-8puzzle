128d22608178a96ef92db3fde1b166fd68961900
cathuyenphan <111101271+cathuyenphan@users.noreply.github.com>
2024-10-27 16:35:24 -0700

    Create operators.h

---
+++ operators.h
@@ -0,0 +1,28 @@
+#ifndef OPERATORS_H_
+#define OPERATORS_H_
+
+#include "puzzle.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> swap(int x, int y, int i, int j)  {
+  //swap two nodes
+};
+
+vector<vector<int>> moveDown(vector<vector<int>> puzzle)  {
+  // move node down
+};
+
+vector<vector<int>> moveUp(vector<vector<int>> puzzle)  {
+  //move node up
+};
+
+vector<vector<int>> moveRight(vector<vector<int>> puzzle)  {
+  // move node right
+};
+
+vector<vector<int>> moveLeft(vector<vector<int>> puzzle)  {
+  // move node left
+};
